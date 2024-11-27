a61722f0b647d03664d58cb86c1c8292797e48c1
JulienRdgs <julien.rodrigues.onze@gmail.com>
2024-11-27 13:57:12 +0100

    changement activeButton() (incomplet)

--- projet prog avancée/main.cpp
+++ projet prog avancée/main.cpp
@@ -26,20 +26,29 @@ const float OUTLINE_THICK = 1.0f;
 
 sf::Color setColor = sf::Color::Black;
 
+std::vector<bool> buttonState;
 bool styloB = false;
 bool pinceauB = false;
 bool rougeB = false;
 bool roseB = false;
 
 void activeButton(sf::RectangleShape button, sf::Mouse mouse, sf::RenderWindow& window, bool& selec) {
-    if (button.getGlobalBounds().contains(sf::Vector2f(mouse.getPosition(window))) && selec == false) {
+    if (button.getGlobalBounds().contains(sf::Vector2f(mouse.getPosition(window)))) {
         std::cout << "dessiner" << std::endl;
         selec = true;
-    }
-    else if (button.getGlobalBounds().contains(sf::Vector2f(mouse.getPosition(window))) && selec == true) {
-        std::cout << "ne plus dessiner" << std::endl;
-        selec = false;
-        setColor = sf::Color::Black;
+        for (bool bouton : buttonState) {
+            if (bouton != selec) {
+                bouton = false; //désactiver tous les autres boutons si après avoir cliqué sur un bouton
+            }
+        }
+        // ALTERNATIVE : modifier manuellement tous les autres états dans un if(etat=true) après chaque activeButton
+
+        for (bool bouton : buttonState) {
+            std::cout << bouton << std::endl;
+        }
+        if (button.getFillColor() != sf::Color::White) { //si le bouton est une couleur, alors la couleur actuelle devient celle-ci
+            setColor = button.getFillColor(); 
+        }
     }
 }
 
@@ -47,7 +56,16 @@ void defaultParam(sf::RectangleShape &colorButton) { // actuellent ça ne save qu
     colorButton.setOutlineThickness(OUTLINE_THICK);
     colorButton.setOutlineColor(sf::Color::Black);
 }
+
+std::vector<sf::CircleShape> dessin;
+std::vector<sf::RectangleShape> boutons;
+
 int main() {
+
+    buttonState.push_back(styloB);
+    buttonState.push_back(pinceauB);
+    buttonState.push_back(rougeB);
+    buttonState.push_back(roseB);
 
     sf::Image image;
     image.create(WINDOW_WIDTH, WINDOW_HEIGHT, sf::Color::White);
@@ -56,7 +74,7 @@ int main() {
     sf::Sprite spriteImage;
     spriteImage.setTexture(textImage);
 
-    std::vector<sf::CircleShape> dessin;
+    
 
     sf::Font font;
     if (!font.loadFromFile("asset/arial.ttf")) { std::cout << "erreur chargement arial" << std::endl; }
@@ -65,45 +83,56 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Paint SFML JRdgs");
     window.setFramerateLimit(60);
 
+    sf::RectangleShape souris(sf::Vector2f(BUTTON_SIZE, BUTTON_SIZE));
+    souris.setPosition(SET_ROW, SET_ROW);
+    souris.setFillColor(sf::Color::White);
+    defaultParam(souris);
+    sf::Texture iconeSouris;
+    if (!iconeSouris.loadFromFile("asset/icone souris.png")) { std::cout << "erreur chargement icone souris" << std::endl; }
+    souris.setTexture(&iconeSouris);
+    boutons.push_back(souris);
+
     sf::RectangleShape stylo(sf::Vector2f(BUTTON_SIZE, BUTTON_SIZE));
-    stylo.setPosition(SET_ROW, SET_ROW);
+    stylo.setPosition(SET_ROW + BUTTON_GAP, SET_ROW);
     stylo.setFillColor(sf::Color::White);
-    stylo.setOutlineThickness(OUTLINE_THICK);
-    stylo.setOutlineColor(sf::Color::Black);
+    defaultParam(stylo);
     sf::Texture iconeStylo;
     if (!iconeStylo.loadFromFile("asset/icone stylo.png")) { std::cout << "erreur chargement icone stylo" << std::endl; }
     stylo.setTexture(&iconeStylo);
+    boutons.push_back(stylo);
 
     sf::RectangleShape pinceau(sf::Vector2f(BUTTON_SIZE, BUTTON_SIZE));
-    pinceau.setPosition(SET_ROW + BUTTON_GAP, SET_ROW);
+    pinceau.setPosition(SET_ROW + BUTTON_GAP*2, SET_ROW);
     pinceau.setFillColor(sf::Color::White);
-    pinceau.setOutlineThickness(OUTLINE_THICK);
-    pinceau.setOutlineColor(sf::Color::Black);
+    defaultParam(pinceau);
     sf::Texture iconePinceau;
     if (!iconePinceau.loadFromFile("asset/icone pinceau.png")) { std::cout << "erreur chargmenent icone pinceau" << std::endl; }
     pinceau.setTexture(&iconePinceau);
+    boutons.push_back(pinceau);
 
     sf::RectangleShape rouge(sf::Vector2f(BUTTON_SIZE, BUTTON_SIZE));
     rouge.setPosition(WINDOW_WIDTH - SET_ROW - BUTTON_SIZE, SET_ROW);
     rouge.setFillColor(sf::Color::Red);
     defaultParam(rouge);
-    /*rouge.setOutlineThickness(OUTLINE_THICK);
-    rouge.setOutlineColor(sf::Color::Black);*/ //ces deux lignes sont faites dans le defaultParam()
+    boutons.push_back(rouge);
 
     sf::RectangleShape bleu(sf::Vector2f(BUTTON_SIZE, BUTTON_SIZE));
     bleu.setPosition(WINDOW_WIDTH - SET_ROW - BUTTON_SIZE - BUTTON_GAP, SET_ROW);
     bleu.setFillColor(sf::Color::Blue);
     defaultParam(bleu);
+    boutons.push_back(bleu);
 
     sf::RectangleShape vert(sf::Vector2f(BUTTON_SIZE, BUTTON_SIZE));
     vert.setPosition(WINDOW_WIDTH - SET_ROW - BUTTON_SIZE - BUTTON_GAP * 2, SET_ROW);
     vert.setFillColor(sf::Color::Green);
     defaultParam(vert);
+    boutons.push_back(vert);
 
     sf::RectangleShape rose(sf::Vector2f(BUTTON_SIZE, BUTTON_SIZE));
     rose.setPosition(WINDOW_WIDTH - SET_ROW - BUTTON_SIZE - BUTTON_GAP * 3, SET_ROW);
     rose.setFillColor(sf::Color(255,0,255));
     defaultParam(rose);
+    boutons.push_back(rose);
 
     sf::VertexArray ligne(sf::Lines);
     ligne.append(sf::Vertex(sf::Vector2f(0, POS_LIGNE), sf::Color::Black));
@@ -130,20 +159,10 @@ int main() {
             if (event.type == sf::Event::MouseButtonReleased) {
 
                 if (event.mouseButton.button == sf::Mouse::Left) {
-                    activeButton(stylo, mouse, window, styloB); //répeter pour chaque bouton
-                    if (styloB) pinceauB = false; // pas réussi a assembler les booléens des boutons dans un vecteur.
+                    activeButton(stylo, mouse, window, styloB);
                     activeButton(pinceau, mouse, window, pinceauB);
-                    if (pinceauB) styloB = false;
                     activeButton(rouge, mouse, window, rougeB);
-                    if (rougeB) {
-                        roseB == false;
-                        setColor = sf::Color::Red;
-                    } //faire une fonction pour éviter de taper le meme activeButton pour chaque couleur ? ou jlaisse genre 4 couleur pour 4 copiés-collés ?
                     activeButton(rose, mouse, window, roseB);
-                    if (roseB) { 
-                        rougeB == false;
-                        setColor = sf::Color(255,0,255); //jsp pk jpeux pas switch du rose au rouge directement.
-                    }
                 }
             }
         }
@@ -165,16 +184,13 @@ int main() {
 
         // Dessiner les éléments
         window.clear(sf::Color::White);
-        window.draw(spriteImage);
-        window.draw(stylo);
-        window.draw(pinceau);
-        window.draw(ligne);
-        window.draw(rouge);
-        window.draw(bleu);
-        window.draw(vert);
-        window.draw(rose);
-        for (sf::CircleShape shape : dessin) {
-            window.draw(shape);
+        window.draw(spriteImage);//l'image pour le setPixel du stylo, a potentiellement changer
+        window.draw(ligne); //la ligne qui sépare les boutons de l'espace de dessin
+        for (sf::RectangleShape &bouton : boutons) {
+            window.draw(bouton);//tous les boutons
+        }
+        for (sf::CircleShape &shape : dessin) {
+            window.draw(shape);//les cercles créés par le pinceau, a potentiellement changer
         }
 
         window.display();
